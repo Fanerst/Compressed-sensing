2c34330700106a99b401fb5bb9295c24e5f35f80
Feng Pan <fan_physics@126.com>
2018-07-25 15:33:46 +0800

    change sample

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ d = 30
 # initialize matrix A and x
 A = np.zeros((M, N))
 x = np.zeros(N)
-x1 = np.zeros(N)
+x_sample = np.zeros([N, 10000])
+y_sample = np.zeros([M, 10000])
 
 with open('data/AAA_Gauss_1000_1000.mtx', 'r') as f1:
     list1 = f1.readlines()
@@ -22,20 +23,27 @@ with open('data/XXX0samples_Gaussian_1000.dat', 'r') as f2:
 f1.close()
 f2.close()
 
+# constructing Gaussian matrix A
 for i in range(2, M+2):
     A[i-2] = list1[i].split()
 for j in range(N):
     A[:, j] = A[:, j] / np.sqrt(sum([k*k for k in A[:, j]]))
 
-
 for i in range(3, d+3):
     temp = list2[i].split()
     x[int(temp[0])-1] = float(temp[1])
 
-for i in range(d+3, 2*d+3):
-    temp = list2[i].split()
-    x1[int(temp[0])-1] = float(temp[1])
+# construct samples of the original vector x and calculate the compressed vector y
+s = np.random.randint(4, 1003, [d, 10000])
+for j in range(10000):
+    for i in s[:, j]:
+        temp = list2[i].split()
+        x_sample[int(temp[0]), j] = float(temp[1])
 
+for i in range(10000):
+    y_sample[:, i] = np.dot(A, x_sample[:, i])
+
+# generating random matrix
 A1 = np.random.random_sample((M, N))
 for i in range(M):
     A1[i, :] = A1[i, :] / np.sqrt(sum([k*k for k in A1[i, :]]))
